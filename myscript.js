843e1738453eb2fa1c64d5e7000095731cc721ef
TAN.TON <nhattan2007@gmail.com>
2020-11-06 09:42:29 +0700

    C2 - Game lan bong ver 2

--- myscript.js
+++ myscript.js
@@ -1,5 +1,6 @@
 var PosX = 0;
 var PosY = 0;
+var rotate = 5;
 
 // Move the ball
 document.getElementById("bntUp").addEventListener("mousedown",moveup);
@@ -7,48 +8,74 @@ document.getElementById("bntDown").addEventListener("mousedown",movedown);
 document.getElementById("bntLeft").addEventListener("mousedown",moveleft);
 document.getElementById("bntRight").addEventListener("mousedown",moveright);
 
+function init(){
+    let the_ball = document.getElementById("the_ball");
+    the_ball.style.top = 0;
+    the_ball.style.left = 0;
+    the_ball.style.position = "absolute";
+    the_ball.style.width = "200px";
+    the_ball.style.height = "200px";
+}
 function moving (x,y){
-    var the_ball = document.getElementById("the_ball")
-    if (PosX < 0){
-        PosX = 0;
+    let the_ball = document.getElementById("the_ball");
+    PosX += x;
+    PosY += y;
+    if (x > 0 | y > 0){
+        rotate += 5;
     }
-    else{
-        if (PosX > screen.width){
-            PosX = screen.width;
-        }
         else{
-            PosX += x;
+            rotate -= 5;
         }
-    }
+    // if (PosX < 0){
+    //     PosX = 0;
+    // }
+    //     else{
+    //         if (PosX > window.innerWidth){
+    //             PosX = window.innerWidth;
+    //         }
+    //             else{
+    //                 PosX += x;
+    //             };
+    //     };
 
-    if (PosY < 0){
-        PosY = 0;
-    }
-    else{
-        if (PosY > screen.height){
-            PosY = screen.height;
-        }
-        else{
-            PosY += y;
-        }
-    }
+    // if (PosY < 0){
+    //     PosY = 0;
+    // }
+    //     else{
+    //         if (PosY > window.innerHeight){
+    //             PosY = window.innerHeight;
+    //         }
+    //             else{
+    //                 PosY += y;
+    //             };
+    //     };
     updatePos();
 }
 
 function updatePos(){
+    let the_ball = document.getElementById("the_ball");
     the_ball.style.top = PosY + "px";
     the_ball.style.left = PosX + "px";
+    the_ball.style.transform = `rotate(${rotate}deg)`;
 }
 
 function moveright(){
-    moving(10,0);
+    if (PosX + parseInt(the_ball.style.width) < innerWidth){
+        moving(10,0);
+    }
 };
 function moveleft(){
-    moving(-10,0);
+    if (PosX > 0){
+        moving(-10,0);
+    }
 };
 function moveup(){
-    moving(0,-10);
+    if (PosY > 0){
+        moving(0,-10);
+    }
 };
 function movedown(){
-    moving(0,10);
+    if (PosY + parseInt(the_ball.style.height) < innerHeight){
+        moving(0,10);
+    }
 };
