b2f9343c996280ac45658b4468b9984629e1ff83
TAN.TON <nhattan2007@gmail.com>
2020-11-10 10:13:12 +0700

    C5 - Update movement by keyboard

--- myscript.js
+++ myscript.js
@@ -1,12 +1,42 @@
 var PosX = 0;
 var PosY = 0;
 var rotate = 5;
-// Move the ball
+
+//set CSS color for button when don't click or press the direction keyboard
+window.addEventListener("mouseup",setColorBtn);
+window.addEventListener("keyup",setColorBtn);
+
+function setColorBtn() {
+    document.getElementById("bntUp").style.backgroundColor = "rgb(228, 228, 228)";
+    document.getElementById("bntDown").style.backgroundColor = "rgb(228, 228, 228)";
+    document.getElementById("bntLeft").style.backgroundColor = "rgb(228, 228, 228)";
+    document.getElementById("bntRight").style.backgroundColor = "rgb(228, 228, 228)";
+}
+
+// Move the ball by mouse
 document.getElementById("bntUp").addEventListener("mousedown",moveup);
 document.getElementById("bntDown").addEventListener("mousedown",movedown);
 document.getElementById("bntLeft").addEventListener("mousedown",moveleft);
 document.getElementById("bntRight").addEventListener("mousedown",moveright);
 
+// Move the ball by direction keyboard
+window.addEventListener("keydown",getKeyCode);
+function getKeyCode(event){
+    var keyCode = event.keyCode;
+    switch (keyCode) {
+        case 38:
+            moveup();
+            break;
+        case 37:
+            moveleft();
+            break;
+        case 39:
+            moveright();
+            break;
+        case 40:
+            movedown();
+    }
+}
 function init(){
     let the_ball = document.getElementById("the_ball");
     the_ball.style.top = 0;
@@ -15,11 +45,11 @@ function init(){
     the_ball.style.width = "200px";
     the_ball.style.height = "200px";
 }
-function moving (x,y){
+function moving (speedX,speedY){
     let the_ball = document.getElementById("the_ball");
-    PosX += x;
-    PosY += y;
-    if (x > 0 | y > 0){
+    PosX += speedX;
+    PosY += speedY;
+    if (speedX > 0 | speedY > 0){
         rotate += 20;
     }
         else{
@@ -36,6 +66,7 @@ function updatePos(){
 }
 
 function moveright(){
+    document.getElementById("bntRight").style.backgroundColor = "rgb(134 134 134)";
     if (PosX + parseInt(the_ball.style.width) < innerWidth){
         let speed = 30;
         if (innerWidth - PosX - parseInt(the_ball.style.width) < speed) {
@@ -45,6 +76,7 @@ function moveright(){
     }
 };
 function moveleft(){
+    document.getElementById("bntLeft").style.backgroundColor = "rgb(134 134 134)";
     if (PosX > 0){
         let speed = 30;
         if (PosX < speed) {
@@ -54,6 +86,7 @@ function moveleft(){
     }
 };
 function moveup(){
+    document.getElementById("bntUp").style.backgroundColor = "rgb(134 134 134)";
     if (PosY > 0){
         let speed = 30;
         if (PosY < speed) {
@@ -63,6 +96,7 @@ function moveup(){
     }
 };
 function movedown(){
+    document.getElementById("bntDown").style.backgroundColor = "rgb(134 134 134)";
     if (PosY + parseInt(the_ball.style.height) < innerHeight){
         let speed = 30;
         if (innerHeight - PosY - parseInt(the_ball.style.height) < speed) {
